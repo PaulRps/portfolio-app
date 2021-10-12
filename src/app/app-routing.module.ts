0e8da41c6966ab22c05e01af1b2c143e0fd59351
paulo.pereira <paulo.pereira@certisign.com.br>
2021-10-12 17:14:10 -0300

    changed redirect default route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ const routes: Routes = [
   { path: 'education', loadChildren: () => import('./modules/education/education.module').then((m) => m.EducationModule) },
   { path: 'experience', loadChildren: () => import('./modules/experience/experience.module').then((m) => m.ExperienceModule) },
   { path: 'projects', loadChildren: () => import('./modules/projects/projects.module').then((m) => m.ProjectsModule) },
-  { path: '', redirectTo: '/about', pathMatch: 'full' },
+  { path: '', redirectTo: 'about', pathMatch: 'full' },
 ];
 
 @NgModule({
