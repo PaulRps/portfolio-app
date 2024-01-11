5eda4cc98fffb15d792f857e89e63780d77b0668
paulRps <paulosilvajp0@gmail.com>
2024-01-11 19:52:59 -0300

    feat: added message to show info when the rerquests take too much time

--- src/app/core/loading-screen/loading-screen.component.ts
+++ src/app/core/loading-screen/loading-screen.component.ts
@@ -1,12 +1,49 @@
-import {Component} from '@angular/core'
+import {Component, OnInit} from '@angular/core'
 import {LoadingService} from './loading.service'
+import {MatSnackBar} from '@angular/material/snack-bar'
 
 @Component({
   selector: 'app-loading-screen',
   templateUrl: './loading-screen.component.html',
   styleUrls: ['./loading-screen.component.scss']
 })
-export class LoadingScreenComponent {
+export class LoadingScreenComponent implements OnInit {
   isLoading = this.loadingService.getLoading()
-  constructor(private readonly loadingService: LoadingService) {}
+  private timeoutId?: any
+  private snackRef: any
+
+  constructor(
+    private readonly loadingService: LoadingService,
+    private snackBar: MatSnackBar
+  ) {}
+  ngOnInit(): void {
+    this.isLoading.subscribe((loading) => {
+      if (loading) {
+        this.startTimeout()
+      } else {
+        this.stopTimeout()
+      }
+    })
+  }
+
+  private toastMessage(msg: string) {
+    this.snackRef = this.snackBar.open(msg, 'close', {
+      horizontalPosition: 'center',
+      verticalPosition: 'bottom',
+      duration: 3600 * 1000
+    })
+  }
+
+  private startTimeout() {
+    this.timeoutId = setTimeout(() => {
+      this.toastMessage(
+        'Sorry for make you wait, the api is hosted in a free server and it takes time to boot it. Stay here to see what i did.'
+      )
+    }, 5000)
+  }
+
+  private stopTimeout() {
+    clearTimeout(this.timeoutId)
+    this.snackRef?.dismiss()
+  }
 }
